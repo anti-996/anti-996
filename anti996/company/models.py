ab1be7e290052527c984b301aa75f2e8c7dc85dc
FingerLiu <liupeng.dalian@gmail.com>
2019-04-09 13:54:21 +0800

    [ADD] add fields

--- anti996/company/models.py
+++ anti996/company/models.py
@@ -17,3 +17,5 @@ class Company(models.Model, AuditMixin):
     name = models.CharField(max_length=255)
     ctype = models.IntegerField(choices=CTYPES)
     desc = models.TextField()
+    homepage = models.URLField(verbose_name="链接", null=True, blank=True)
+    evidence = models.ImageField(null=True, blank=True)
